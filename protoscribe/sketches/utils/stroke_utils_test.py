47e6598577b27d7436b86bd9c57923707b6c7919
Alexander Gutkin <agutkin@google.com>
2025-02-19 22:25:39 +0000

    API for converting regular strokes to stroke-3 format.
    The regular strokes are simply a sequence of a sequence of points. The stroke-3
    format is a tensor (L, 3), where each vector represents a delta from the
    previous x and y with a boolean indicating whether the stylus touches the paper
    or is lifted from it.
    
    PiperOrigin-RevId: 728816645

--- protoscribe/sketches/utils/stroke_utils_test.py
+++ protoscribe/sketches/utils/stroke_utils_test.py
@@ -18,7 +18,6 @@ import json
 import logging
 import os
 
-from absl import flags
 from absl.testing import absltest
 import numpy as np
 from protoscribe.glyphs import make_text
@@ -27,8 +26,6 @@ from protoscribe.sketches.utils import stroke_utils as lib
 
 import glob
 import os
-
-FLAGS = flags.FLAGS
 
 _POINTS_PER_SEGMENT = 10
 
@@ -42,7 +39,11 @@ class StrokeUtilsTest(absltest.TestCase):
   def test_concrete_svg_build_time_stroke_points(self):
     """Tests the point generation build-time API."""
 
-    glyph_path = os.path.join(FLAGS.test_srcdir, _TEST_DATA_DIR, "field.svg")
+    glyph_path = os.path.join(
+        absltest.get_default_test_srcdir(),
+        _TEST_DATA_DIR,
+        "field.svg"
+    )
     _, svg_for_strokes, _, _ = make_text.concat_svgs(
         [glyph_path], ["field"]
     )
@@ -76,7 +77,9 @@ class StrokeUtilsTest(absltest.TestCase):
 
     # Loads strokes from n-best list of stroke sequences in stroke-3 format.
     path = os.path.join(
-        FLAGS.test_srcdir, _TEST_DATA_DIR, "cherry_nbest_strokes3.json"
+        absltest.get_default_test_srcdir(),
+        _TEST_DATA_DIR,
+        "cherry_nbest_strokes3.json"
     )
     with open(path, mode="r") as f:
       stroke_dict = json.loads(f.read())
@@ -88,12 +91,62 @@ class StrokeUtilsTest(absltest.TestCase):
     # Supply `--test_tmpdir=/tmp` to examine the resulting SVG.
     for scale_factor in  [1.0, 0.5, 1.5]:
       svg_filename = os.path.join(
-          FLAGS.test_tmpdir, f"strokes_scale{scale_factor}.svg"
+          absltest.get_default_test_tmpdir(), f"strokes_scale{scale_factor}.svg"
       )
       logging.info("Saving strokes to `%s` ...", svg_filename)
       lib.stroke3_strokes_to_svg_file(
           strokes, svg_filename, scale_factor=scale_factor
       )
+
+  def test_stroke3_from_strokes(self):
+    """Tests conversion of regular strokes to stroke-3 format."""
+
+    strokes = [
+        [(1., 1.), (3., 3.), (6., 6.)],
+        [(8., 8.), (5., 5.), (4., 4.), (2., 2.), (0., 0.)],
+    ]
+    strokes3 = lib.stroke3_from_strokes(
+        strokes, convert_to_deltas=False
+    ).tolist()
+    self.assertEqual(
+        strokes3, [
+            [1., 1., 0.],
+            [3., 3., 0.],
+            [6., 6., 1.],
+            [8., 8., 0.],
+            [5., 5., 0.],
+            [4., 4., 0.],
+            [2., 2., 0.],
+            [0., 0., 1.],
+        ]
+    )
+    strokes3_deltas = lib.stroke3_from_strokes(
+        strokes, convert_to_deltas=True
+    ).tolist()
+    self.assertEqual(
+        strokes3_deltas, [
+            [1., 1., 0.],
+            [2., 2., 0.],
+            [3., 3., 1.],
+            [2., 2., 0.],
+            [-3., -3., 0.],
+            [-1., -1., 0.],
+            [-2., -2., 0.],
+            [-2., -2., 1.],
+        ]
+    )
+
+    test_strokes = [strokes3_deltas[0]]
+    for i in range(1, len(strokes3_deltas)):
+      d_x, d_y, lift_pen = strokes3_deltas[i]
+      test_strokes.append(
+          [
+              test_strokes[i - 1][0] + d_x,
+              test_strokes[i - 1][1] + d_y,
+              lift_pen
+          ]
+      )
+    self.assertEqual(test_strokes, strokes3)
 
 
 if __name__ == "__main__":
