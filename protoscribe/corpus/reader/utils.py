92ac12c9d57aa7063c36905e66646920fac8115f
Alexander Gutkin <agutkin@google.com>
2025-03-14 10:08:50 +0000

    Removing exception branches from the padding function.
    PiperOrigin-RevId: 736801741

--- protoscribe/corpus/reader/utils.py
+++ protoscribe/corpus/reader/utils.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 
 
 def pad_or_trim_sequence(
-    inputs: tf.Tensor, max_sequence_length: int, allow_trimming: bool = True
+    inputs: tf.Tensor, max_sequence_length: int
 ) -> tf.Tensor:
   """Pads the 1-D or 2-D inputs as necessary.
 
@@ -28,29 +28,18 @@ def pad_or_trim_sequence(
     inputs: Input tensor with dimension (L,) or (L, D), where L is the length
       of the sequence.
     max_sequence_length: Maximum sequence to pad to.
-    allow_trimming: If the requested sequence length is shorter than the actual
-      sequence length perform trimming of the input sequence.
 
   Returns:
     Padded tensor.
-
-  Raises:
-    ValueError if rank of the tensor is not supported or trimming is
-    not allowed.
   """
   pad_amount = max_sequence_length - tf.shape(inputs)[0]
   if pad_amount < 0:
-    if allow_trimming:
-      return inputs[:pad_amount, ...]
-    else:
-      raise ValueError(f"Trimming is disabled (amount: {pad_amount})")
+    return inputs[:pad_amount, ...]
 
-  rank = tf.rank(inputs)
+  rank = len(tf.shape(inputs))  # For some reason `tf.rank` doesn't work here.
   if rank == 2:
     paddings = [[0, pad_amount], [0, 0]]
-  elif rank == 1:
+  else:  # Assume 1-D.
     paddings = [[0, pad_amount]]
-  else:
-    raise ValueError("Unsupported tensor rank!")
 
   return tf.pad(inputs, paddings, mode="CONSTANT", constant_values=0)
